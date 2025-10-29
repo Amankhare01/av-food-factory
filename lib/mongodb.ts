08694cf487c48b67492225d80ca0f6ca1b707dcd
codebyhimanshu <himanshusingh993567@gmail.com>
2025-10-29 16:26:01 +0530

    refactor: comment out connection checks and logging in connectDB function for cleaner code

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,19 +3,19 @@ import mongoose from "mongoose";
 export async function connectDB() {
   console.log("🔌 connectDB called"); // ✅ prove entry
   console.log("process env = ",process.env.MONGODB_URI);
-  try {
-    if (mongoose.connection.readyState >= 1) {
-      console.log("🔁 already connected");
-      return;
-    }
+  // try {
+  //   if (mongoose.connection.readyState >= 1) {
+  //     console.log("🔁 already connected");
+  //     return;
+  //   }
 
     const uri = process.env.MONGODB_URI;
     if (!uri) throw new Error("❌ no MONGODB_URI");
     await mongoose.connect(uri);
-    console.log("✅ MongoDB connected successfully");
-  } catch (err) {
-    console.error("❌ MongoDB connection error:", err);
-  }
+  //   console.log("✅ MongoDB connected successfully");
+  // } catch (err) {
+  //   console.error("❌ MongoDB connection error:", err);
+  // }
 }
  
 
